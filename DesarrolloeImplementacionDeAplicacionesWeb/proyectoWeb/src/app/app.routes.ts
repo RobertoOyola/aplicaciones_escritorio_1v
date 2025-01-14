106d5b88269f49f24177f2172b4651f06f3cc6cf
Roberto Oyola Vera <roberto.oyola@tecnologicoliceocristiano.edu.ec>
2025-01-13 21:14:09 -0500

    a

--- DesarrolloeImplementacionDeAplicacionesWeb/proyectoWeb/src/app/app.routes.ts
+++ DesarrolloeImplementacionDeAplicacionesWeb/proyectoWeb/src/app/app.routes.ts
@@ -7,14 +7,23 @@ import { CrearActoresComponent } from './actores/crear-actores/crear-actores.com
 import { IndiceCineComponent } from './cine/indice-cine/indice-cine.component';
 import { CrearCineComponent } from './cine/crear-cine/crear-cine.component';
 import { CrearPeliculaComponent } from './peliculas/crear-pelicula/crear-pelicula.component';
+import { EditarGenerosComponent } from './generos/editar-generos/editar-generos.component';
+import { EditarActoresComponent } from './actores/editar-actores/editar-actores.component';
+import { EditarCineComponent } from './cine/editar-cine/editar-cine.component';
+import { EditarPeliculasComponent } from './peliculas/editar-peliculas/editar-peliculas.component';
 
 export const routes: Routes = [
     {path: '', component:LandingPageComponent},
     {path: 'Generos', component:IndiceGenerosComponent},
-    {path: 'Generos/Crear', component:CrearGenerosComponent},
+    {path: 'Generos/Crear/', component:CrearGenerosComponent},
+    {path: 'Generos/Editar/:id', component:EditarGenerosComponent},
     {path: 'actores', component:IndiceActoresComponent},
     {path: 'actores/crear', component:CrearActoresComponent},
+    {path: 'actores/editar/:id', component:EditarActoresComponent},
     {path: 'cines', component:IndiceCineComponent},
     {path: 'cines/crear', component:CrearCineComponent},
-    {path: 'peliculas/crear', component:CrearPeliculaComponent}
+    {path: 'cines/editar/:id', component:EditarCineComponent},
+    {path: 'peliculas/crear', component:CrearPeliculaComponent},
+    {path: 'peliculas/editar/:id', component:EditarPeliculasComponent},
+    {path: '**', component:LandingPageComponent},
 ];
