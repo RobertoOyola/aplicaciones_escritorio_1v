b53825c3b897dc15dba88eeb7043960b996d25bd
Roberto Oyola Vera <roberto.oyola@tecnologicoliceocristiano.edu.ec>
2025-01-28 20:06:56 -0500

    a

---
+++ DesarrolloeImplementacionDeAplicacionesWeb/proyectoWeb/src/app/compartidos/componentes/mapas/mapas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mapas',
+  imports: [],
+  templateUrl: './mapas.component.html',
+  styleUrl: './mapas.component.css'
+})
+export class MapasComponent {
+
+}
